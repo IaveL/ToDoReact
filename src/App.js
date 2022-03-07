026be5371c061e5307ab1c5b392b3e5abd0a590a
Igor Avelino <igoravel21@gmail.com>
2022-03-07 20:05:00 -0300

    uploading challenge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class ToDo extends Component {
+  state = {
+  task: "",
+  taskList: []
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      task: event.target.value
+    })
+  }
+
+  addTask = (event) => {
+    let {task, taskList} = this.state
+    if(task.length > 0){
+    this.setState({
+      taskList: taskList.concat({
+        task: task,
+        id: Date.now()
+      }),
+      task: ''
+    })
+  }
+  event.preventDefault()
+  }
+
+  removeTask = (id, event) => {
+    
+    let {task, taskList} = this.state
+    this.setState({
+      taskList: taskList.filter((item) => {
+        return item.id != id;
+      })
+    })
+  }
+
+  render(){
+    return(
+      <form className="MainBox" onSubmit={this.addTask}>
+      <div className="Input">
+      <h1>Tasks to do!</h1>
+      <input  type="text" value={this.state.task} placeholder="Type something to do..." 
+onChange={this.handleChange} 
+      />
+      <button className="TaskAdd">Add task</button>
+      </div>
+
+      <div className="Tasks">
+      {this.state.taskList.map((item) => (
+        <ul className="TaskItems">
+          <li>{item.task}</li>
+          <li><button className="TaskRemover" 
+          onClick={()=>{this.removeTask(item.id)}}
+          >X</button></li>
+        </ul>
+      ))}
+      </div>
+      </form>
+    )
+  }
 }
 
-export default App;
+
+export default ToDo;
